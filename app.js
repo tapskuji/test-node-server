d96ed944da404825cbe618c4527372627c5058e8
TAPIWANASHE KUJINGA <80486167+tapskuji@users.noreply.github.com>
2023-06-01 08:56:42 +0200

    adds jobs route

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ dotenv.config({path: './config/config.env'});
 const PORT = process.env.PORT;
 const APP_ENV = process.env.APP_ENV;
 
+// import routes
+const jobs = require('./routes/jobs');
+
+// use routes
+app.use('/api/v1', jobs);
+
 app.listen(PORT, () => {
     console.log(`Server on port ${PORT} in ${APP_ENV} mode.`);
 });
