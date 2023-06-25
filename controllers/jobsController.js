566d46c11c7254cf165f277ee79d5c328df4b096
TAPIWANASHE KUJINGA <80486167+tapskuji@users.noreply.github.com>
2023-06-25 07:16:18 +0200

    adds feature to get all jobs

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,17 +1,19 @@
 const Job = require('../models/jobs');
 
 // get all jobs => /api/v1/jobs
-exports.getJobs = (req, res, next) => {
+exports.getJobs = async (req, res, next) => {
+    const jobs = await Job.find({});
+
     res.status(200).json({
         success: true,
-        username: req.username,
-        message: "Display all jobs"
+        results: jobs.length,
+        data: jobs
     });
 };
 
 // create new job => /api/v1/jobs
 exports.createJob = async (req, res, next) => {
-    console.log(req.body);
+    //console.log(req.body);
     const job = await Job.create(req.body);
 
     res.status(200).json({
